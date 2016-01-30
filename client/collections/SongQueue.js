4d7c25cc87b3936d8575efa8cfa4ddec54bd8038
alexanthony813 <alexanderanthony813@gmail.com>
2016-01-29 17:06:59 -0800

    Implement ended action

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,31 @@
 var SongQueue = Songs.extend({
 
   initialize: function() {
+  	//keeps track of songs
+    console.log("here is songqueue")
+    console.dir(this);
+    this.on('enqueue', function(song){
+      //if it was first song then play
+      console.log('enqueueing');
+      console.dir(this);
+      //regardless, add to songs
+      this.add(song);
+    },this);
+  	//plays first song when it is the only song
+
+  	this.on('ended', this.ended, this);
+  },
+
+  playFirst: function() {
+    var song = this.at(0);
+    song.play();
+  },
+
+  ended: function() {
+  	var song = this.shift();
+  	if (this.at(0)) {
+  		this.playFirst();
+  	}
   }
 
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -16,15 +16,6 @@ var AppModel = Backbone.Model.extend({
     params.library.on('play', function(song) {
       this.set('currentSong', song);
     }, this);
-
-    params.library.on('enqueue',function(song){
-      this.get('songQueue').add(song);
-    }, this);
-
-    params.library.on('ended', function(){
-      var song = this.get('songQueue').shift();
-      song.play();
-    }, this);
   }
 
 });
